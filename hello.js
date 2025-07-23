46e09d6a7031125ddf7b38d1c74706146e730e85
gudenaveen80 <gudenaveen80@gmail.com>
2025-07-23 21:37:10 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("i will 😁 throughout this course");
+// akka one plate idli--akka is object ,idli is method...
+// console is one type of object whereas log is method
+// 2024 js has edition in the name with ES15
+// dom apis provide apis to the core javascript.
+// dom apis allows js to interact with html,css..
+
