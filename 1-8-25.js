4be1873bea6c8c3d44d93cb6af25d5b708108bb6
gudenaveen80 <gudenaveen80@gmail.com>
2025-08-01 21:51:03 +0530

    Add files via upload

---
+++ 1-8-25.js
@@ -0,0 +1,35 @@
+var naveen=1000;
+var anusha=2000;
+var anusha=3000;
+var anusha=4000;
+let Naveen=1000;
+
+console.log(typeof y)
+
+console.log(typeof anu)
+console.log(typeof(y))
+console.log(typeof typeof big)
+console.log(typeof typeof naveen)
+
+var title="alchemist";
+var price=200;
+var pages=250;
+var author="paulo coehlo";
+var anu=10;
+typeof 200;
+console.log(typeof 200)
+console.log(typeof 250)
+console.log(typeof "paulo coehlo")
+console.log(typeof "alchemist")
+
+var naveen=null;
+console.log(typeof null)
+
+let big =124352435765412435466544546565n;
+console.log(typeof big)
+// let nav= symbol("id");
+// console.log(typeof sym)
+
+let y;
+
+let 
