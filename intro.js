695e40122dfbce48d1d0dfda6d200c4805689960
gudenaveen80 <gudenaveen80@gmail.com>
2025-11-04 11:01:34 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,16 @@
+console.log("hey man ")
+console.log(num1)
+var num1= 10
+
+let num2=20
+console.log(num2)
+const num3=30
+console.log(num3)
+
+const name="naveen"
+var city='hyderabad'
+var city='bangalore'
+let age=22
+console.log(age)
+console.log(city)
+console.log(name)
